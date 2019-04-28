3ad378333ebc5af92460c0a7e1b268d974877a10
Nachwahl <robin@ferch-mail.de>
2019-04-28 09:16:06 +0200

    Added Test Level Data

---
+++ assets/Overworld.tsx
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
+ <image source="../../../../Desktop/Overworld.png" width="640" height="576"/>
+ <tile id="44">
+  <objectgroup draworder="index">
+   <object id="1" x="15.75" y="10">
+    <polygon points="0,0 -3,0 -3,0.75 -5.75,1 -9.25,6.125 0.125,6"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="9.875">
+    <polygon points="0,0 3,0 6.75,5.875 -0.25,5.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <objectgroup draworder="index">
+   <object id="1" x="5.75" y="0.25">
+    <polygon points="0,0 0.375,5.625 -0.75,5.875 -0.75,9.625 0.25,9.625 0.25,12.25 9.875,12 10.25,-0.375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <objectgroup draworder="index">
+   <object id="1" x="6.625" y="0">
+    <polygon points="0,0 -0.125,0.875 3,1.125 5.125,3.125 5.625,6.625 6.125,7.375 0.5,12.875 -6.875,13 -6.625,0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="203">
+  <objectgroup draworder="index">
+   <object id="1" x="6.25" y="0.125">
+    <polygon points="0,0 -4.25,3.625 -4,12 -0.25,15.875 7.75,15.75 9.625,14.625 9.625,0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="204">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="0.25">
+    <polygon points="0,0 16,1 16.125,16 0.25,14.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="205">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.5" y="1.125">
+    <polygon points="0,0 3.625,0.125 3.5,-1.125 10.5,-1 10.625,-0.25 12.5,-0.125 12.75,1 13.75,1 13.5,2.75 14.375,2.75 14.5,10.625 10.25,14.875 0.5,14.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="206">
+  <objectgroup draworder="index">
+   <object id="1" x="4.625" y="7.25">
+    <polygon points="0,0 0.125,-0.875 1.125,-1.125 1.125,-2.5 5.625,-2.125 5,-1.125 7.25,-1.25 7.25,0.375 8.25,0.375 9.25,1.625 9.125,2.75 7.75,4.25 6.25,5 3.25,5 1.125,3.5 -0.75,3.125 -1,1.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="207">
+  <objectgroup draworder="index">
+   <object id="1" x="1.75" y="6.875">
+    <polygon points="0,0 0,-3.5 2.25,-5.875 5.125,-6 10.375,-4.625 13,-1.875 13.375,3.625 10.875,6 1.375,6 -0.125,5.25 -1.125,4.25 -1.375,2.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1436">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0.272727">
+    <polygon points="0,0 15.9091,-0.272727 15.8182,15.6364 -0.181818,15.6364"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/characters.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="characters" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="D:/Klengan The Game/klenganassets.png" width="128" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="12.9375" y="31">
+    <polygon points="0,0 0.1875,-31 18.25,-30.9375 18.4375,0.875 -0.125,0.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="11.1875" y="0.1875">
+    <polygon points="0,0 0.5625,31.8125 19.4375,31.8125 19.5625,-0.3125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="3" duration="500"/>
+  </animation>
+ </tile>
+</tileset>
